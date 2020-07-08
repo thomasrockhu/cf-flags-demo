5a41897b4ef043b2cf6c0224004bc050e455ab61
Eli Hooten <elir.hooten@gmail.com>
2020-07-08 15:50:49 -0500

    Add New Functions and Test to Demo
    Also fixes pasting error with .gitignore.

--- src/demo/demo.py
+++ src/demo/demo.py
@@ -4,3 +4,12 @@ class Demo(object):
 
     def tested_function(self):
         return True
+    
+    def partial_function(self, check):
+        if check:
+            return True
+        else:
+            return False
+    
+    def untested_function_two(self):
+        return True
--- tests/unit/demo/test_demo.py
+++ tests/unit/demo/test_demo.py
@@ -3,3 +3,6 @@ from src.demo.demo import Demo
 
 def test_tested_function():
     assert Demo().tested_function()
+
+def test_partial_function():
+    assert Demo().partial_function(True)
